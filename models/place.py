d0356d41f8cef678b9fd6d37e78eaba55ac3b296
Lvmvr22 <lamarnyairo@gmail.com>
2021-08-11 06:23:58 +0300

    airbnb2

---
+++ models/place.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+import models
+from models.base_model import BaseModel
+from os import getenv
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
